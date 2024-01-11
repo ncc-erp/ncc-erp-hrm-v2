9af0855453ba004671b03caff5085e27f935a981
trungnghiancc <nghia.giangtrung@ncc.asia>
2024-01-11 16:49:46 +0700

    init Dtos for API get salary chart data

---
+++ aspnet-core/src/HRMv2.Core/Manager/Home/Dtos/ChartDto/PayslipChartDto.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Application.Services.Dto;
+using HRMv2.Entities;
+using HRMv2.Manager.Categories.Charts.ChartDetails.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HRMv2.Constants.Enum.HRMEnum;
+
+namespace HRMv2.Manager.Home.Dtos.ChartDto
+{
+    public class PayslipChartDto : EntityDto<long>
+    {
+        public long EmployeeId { get; set; }
+        public string FullName { get; set; }
+        public double Salary { get; set; }
+        public Sex Gender { get; set; }
+        public long BranchId { get; set; }
+        public long JobPositionId { get; set; }
+        public long LevelId { get; set; }
+        public List<long> TeamIds { get; set; }
+        public UserType UserType { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+
+}
---
+++ aspnet-core/src/HRMv2.Core/Manager/Home/Dtos/ChartDto/PayslipDetailChartDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services.Dto;
+using HRMv2.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HRMv2.Constants.Enum.HRMEnum;
+
+namespace HRMv2.Manager.Home.Dtos.ChartDto
+{
+    public class PayslipDetailChartDto : EntityDto<long>
+    {
+        public long PayslipId { get; set; }
+        public double Money { get; set; }
+        public PayslipDetailType Type { get; set; }
+        public long? ReferenceId { get; set; }
+    }
+}
