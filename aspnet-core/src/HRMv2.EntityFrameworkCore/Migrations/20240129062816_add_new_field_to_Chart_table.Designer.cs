de43ef1e6394eac0647cfcf5c49eebbc305aa379
baovuduc <bao.vuduc@ncc.asia>
2024-01-29 13:29:53 +0700

    migration: add new field to chart table

--- aspnet-core/src/HRMv2.EntityFrameworkCore/Migrations/20240129062816_add_new_field_to_Chart_table.Designer.cs
+++ aspnet-core/src/HRMv2.EntityFrameworkCore/Migrations/20240129062816_add_new_field_to_Chart_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HRMv2.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HRMv2.Migrations
 {
     [DbContext(typeof(HRMv2DbContext))]
-    partial class HRMv2DbContextModelSnapshot : ModelSnapshot
+    [Migration("20240129062816_add_new_field_to_Chart_table")]
+    partial class add_new_field_to_Chart_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/HRMv2.EntityFrameworkCore/Migrations/20240129062816_add_new_field_to_Chart_table.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMv2.Migrations
+{
+    public partial class add_new_field_to_Chart_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShareToUserIds",
+                table: "Charts",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "shareToRoleIds",
+                table: "Charts",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShareToUserIds",
+                table: "Charts");
+
+            migrationBuilder.DropColumn(
+                name: "shareToRoleIds",
+                table: "Charts");
+        }
+    }
+}
