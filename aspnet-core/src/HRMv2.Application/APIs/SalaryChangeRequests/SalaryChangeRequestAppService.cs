ae6d6275debd4057867255e656980d02268e00ac
dat.dotien <dat.dotien@ncc.asia>
2025-01-15 11:30:47 +0700

    update

--- aspnet-core/src/HRMv2.Application/APIs/SalaryChangeRequests/SalaryChangeRequestAppService.cs
+++ aspnet-core/src/HRMv2.Application/APIs/SalaryChangeRequests/SalaryChangeRequestAppService.cs
@@ -181,7 +181,7 @@ namespace HRMv2.APIs.SalaryChangeRequests
           return  await _salaryRequestManager.CreateSalaryChangeRequestFromCheckpointTool(input);
         }
 
-        [HttpGet]
+        [HttpPost]
         [NccAuthentication]
         public async Task<List<EmployeeSalaryInfo>> GetEmployeeSalaryInfo(List<string> employeeEmails)
         {
