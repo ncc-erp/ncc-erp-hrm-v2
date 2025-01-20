fb594ae7d4743428f650efa2790e33910e772c67
dat.dotien <dat.dotien@ncc.asia>
2025-01-20 18:01:03 +0700

    update Notify ini Mezon

---
+++ aspnet-core/src/HRMv2.Core/Manager/Notifications/NotifyToChannel/Dto/MezonMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMv2.Manager.Notifications.NotifyToChannel.Dto
+{
+    public class InputMezonMessage
+    {
+        public string type { get; set; }
+        public MezonMessage message { get; set; }
+    }
+    public class MezonMessage
+    {
+        public string username { get; set; }
+        public string t { set; get; }
+        public List<MezonMessageMention> mentions { set; get; }
+    }
+
+    public class MezonMessageMention
+    {
+        public string username { set; get; }
+        public int s { set; get; }
+        public int e => s + username.Length +1;
+    }
+}
