18560feb218bb9bbe757243b0bacb74f578b7365
quandaominh <quan.daominh@ncc.asia>
2025-06-16 10:09:50 +0700

    feat : change api get-pay-roll and display status

--- aspnet-core/src/HRMv2.Core/Manager/Salaries/Payrolls/Dto/PayrollResultQuery.cs
+++ aspnet-core/src/HRMv2.Core/Manager/Salaries/Payrolls/Dto/PayrollResultQuery.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static HRMv2.Constants.Enum.HRMEnum;
 
 namespace HRMv2.Manager.Salaries.Payrolls.Dto
 {
@@ -10,6 +11,28 @@ namespace HRMv2.Manager.Salaries.Payrolls.Dto
     {
         public long Value { get; set; }
         public DateTime ApplyDate { get; set; }
+
+        public PayrollStatus Status { get; set; }
         public string Name => ApplyDate.ToString("yyyy-MM");
+
+        public string StatusName
+        {
+            get
+            {
+                return Status switch
+                {
+                    PayrollStatus.New => "New",
+                    PayrollStatus.PendingKT => "Pending KT",
+                    PayrollStatus.RejectedByKT => "Rejected by KT",
+                    PayrollStatus.PendingCEO => "Pending CEO",
+                    PayrollStatus.ApprovedByCEO => "Approved by CEO",
+                    PayrollStatus.RejectedByCEO => "Rejected by CEO",
+                    PayrollStatus.Executed => "Executed",
+                    _ => "",
+                };
+            }
+        }
+
+
     }
 }
