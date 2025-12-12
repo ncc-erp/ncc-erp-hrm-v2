21a6c2e862c2bb4000e33aa1d4dbfd5db708242e
Son Nguyen Hong <son.nguyenhong1@ncc.asia>
2025-12-12 14:00:39 +0700

    Auto change Currency

--- aspnet-core/src/HRMv2.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
+++ aspnet-core/src/HRMv2.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
@@ -40,6 +40,7 @@ namespace HRMv2.EntityFrameworkCore.Seed
                 new TenantRoleAndUserBuilder(context, id).Create();
                 new TenantLevelBuilder(context, id).Create();
                 new TenantEmailTemplateBuilder(context, id).Create();
+                new DebtCurrencyFixed(context, id).Fixed();
             }
         }
 
---
+++ aspnet-core/src/HRMv2.EntityFrameworkCore/EntityFrameworkCore/Seed/Tenants/DebtCurrencyFixed.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMv2.EntityFrameworkCore.Seed.Tenants
+{
+    public class DebtCurrencyFixed
+    {
+        private readonly HRMv2DbContext _context;
+        private readonly int _tenantId;
+        public DebtCurrencyFixed(HRMv2DbContext context, int tenantId)
+        {
+            _context = context;
+            _tenantId = tenantId;
+        }
+
+        public void Fixed()
+        {
+            var debts = _context.Debts
+                .Where(d => d.TenantId == _tenantId && d.Currency == null)
+                .ToList();
+
+            if (!debts.Any())
+                return;
+
+            foreach (var debt in debts)
+            {
+                debt.Currency = "VND";
+            }    
+
+            _context.SaveChanges();
+        }
+    }
+}
